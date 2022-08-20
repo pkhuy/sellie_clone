2493782421862b464050a6e76ff954634e432c27
pkhuy <57100865+pkhuy@users.noreply.github.com>
2022-08-21 03:07:56 +0700

    base

---
+++ models/customer.py
@@ -0,0 +1,7 @@
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
