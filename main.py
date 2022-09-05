ae9dec4c5919e6f3c0015b67833a7ddc6c949af0
huypk <huypk239@gmail.com>
2022-09-05 18:31:59 +0700

    add  base at com

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-import os
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
-from flask import Flask, flash, request, jsonify, render_template, redirect
-from flask.helpers import url_for
-from service.auth import Auth
-from flask_login import current_user, LoginManager, login_user, current_user, logout_user, login_required
-from routes.routes import api_bp, ui_bp
-from https.api.auth_bp import secret_key
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), None)
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -23,44 +35,11 @@ def index():
 
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        return render_template('login.html', tasks=tasks)
 
-@app.route('/login', methods=['GET'])
-def index():
-    if request.method == 'POST':
-        email = request.form["email"]
-        password = request.form['password']
-        if true:
-            return
-    return render_template('login.html', tasks=tasks)
 
-@app.router('/register', methods['POST'])
-def register():
-    if request.method == 'POST':
-        email = request.form["email"]
-        password = request.form['password']
-        confirm_password = request.form['confirm_password']
-        return
-    return render_teamplate('register.html')
-
-@app.route('/customers', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
-
-@app.route('/vendors')
-def add_vendor():
+@app.route('/delete/<int:id>')
+def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
 
     try:
@@ -70,11 +49,7 @@ def add_vendor():
     except:
         return 'There was a problem deleting that task'
 
-
-
-@app.route('/partners', methods=['GET', 'POST'])
-def create():
-    return 
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
 
@@ -90,135 +65,22 @@ def update(id):
     else:
         return render_template('update.html', task=task)
 
-@app.route('/partners/<int:id>', methods=['PUT', 'DELETE'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
+@app.route('/vendors', methods=['POST', 'GET'])
+def get_all_vendors():
     if request.method == 'POST':
-        task.content = request.form['content']
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
 
         try:
+            db.session.add(new_task)
             db.session.commit()
             return redirect('/')
         except:
-            return 'There was an issue updating your task'
+            return 'There was an issue adding your task'
 
     else:
-        return render_template('update.html', task=task)
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
-@app.route('/customers', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
-
-        @app.route('/customers', methods=['GET', 'POST'])
-
-@app.route('/customers', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
-
-
-@app.route('/customers', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
-
-@app.route('/customers', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
-
-@app.route('/customers', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
-
-@app.route('/customers', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
-
-def init_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = secret_key
-    app.register_blueprint(api_bp, url_perfix="/api/v1")
-    app.register_blueprint(ui_bp, url_prefix="")
-    login_manager = LoginManager(app)
-    login_manager.login_view = 'login'
-    login_manager.login_message_category = 'info'
-    
-    @login_manager.user_loader
-    def load_user(user_id):
-        return Auth().loaded_user(user_id)
-    
-    app.run()
-
-if __name__ == '__main__':
-    init_app()
+if __name__ == "__main__":
+    app.run(debug=True)
--- models/customer.py
+++ models/customer.py
@@ -1,3 +1,5 @@
+
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
